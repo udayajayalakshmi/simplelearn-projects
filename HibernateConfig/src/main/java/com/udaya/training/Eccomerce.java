2a17277c4aba908376bd84d1373f12cee891e636
udayajayalakshmi <97719218+udayajayalakshmi@users.noreply.github.com>
2022-03-18 16:35:53 +0530

    Add files via upload

---
+++ HibernateConfig/src/main/java/com/udaya/training/Eccomerce.java
@@ -0,0 +1,24 @@
+package com.udaya.training;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+public class Eccomerce {
+	static {
+        try {
+                StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                                .configure("hibernate.cfg.xml").build();
+                Metadata metaData = new MetadataSources(standardRegistry).getMetadataBuilder().build();
+                sessionFactory = metaData.getSessionFactoryBuilder().build();
+        } catch (Throwable th) {
+                throw new ExceptionInInitializerError(th);
+        }
+}
+
+public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+}
+}
+	
+		
